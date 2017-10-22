cdea4d89573e304a911e3cc509f6489ecb7174c4
Simon Schürrle <sitischu@hotmail.de>
2017-10-22 14:22:37 +0200

    Create file with the SQL for the DB

---
+++ sourukorekuta/dbsetup.sql
@@ -0,0 +1,33 @@
+CREATE SCHEMA sourukorekuta;
+CREATE TABLE sourukorekuta.groups
+(
+    id TEXT,
+    name TEXT,
+    channel_id TEXT,
+    access_hash TEXT
+);
+CREATE TABLE sourukorekuta.statistics
+(
+    user_id TEXT,
+    group_id TEXT,
+    count_text INTEGER,
+    count_mention INTEGER,
+    count_hashtag INTEGER,
+    count_bot_command INTEGER,
+    count_url INTEGER,
+    count_email INTEGER,
+    count_photo INTEGER,
+    count_sticker INTEGER,
+    count_gif INTEGER,
+    count_video INTEGER,
+    count_voice INTEGER,
+    joindate INTEGER,
+    count_document INTEGER
+);
+CREATE TABLE sourukorekuta.users
+(
+    id TEXT,
+    first_name TEXT,
+    last_name TEXT,
+    username TEXT
+);
