55178d9bb5905dac6dbd1cf212bf3fd2f1f2a409
Simon Schürrle <sitischu@hotmail.de>
2017-10-27 23:03:35 +0200

    Commit all needed Files

---
+++ sourukorekuta/dbops.py
@@ -0,0 +1,233 @@
+import userbot
+import config
+from telegram import Chat, Bot, ChatMember
+from telegram.user import User
+import psycopg2 as ps2
+from psycopg2.extensions import connection as ps2conn
+from psycopg2.extensions import cursor as ps2cursor
+
+curdir = '/'.join(__file__.split('/')[:-1])
+
+
+def runsql(sql: str, data: tuple = (), mogrify: bool = False) -> str:
+    conn_string = f"""host='{config.db_host}' dbname='{config.db_name}'
+                     user='{config.db_user}' password='{config.db_password}' port='{config.db_port}'"""
+    conn = ps2.connect(conn_string)  # type: ps2conn
+    c = conn.cursor()  # type: ps2cursor
+    if mogrify:
+        return c.mogrify(sql, data)
+    c.execute(sql, data)
+    # result = c.mogrify(sql, data)
+    try:
+        result = c.fetchall()
+    except ps2.ProgrammingError:
+        result = None
+    conn.commit()
+    return result
+
+
+def addgroup(chat: Chat) -> None:
+    if chat.type != 'private':
+        chatname = chat.title
+    else:
+        return
+    chatid = str(chat.id).strip()
+
+    if not runsql("SELECT id FROM sourukorekuta.groups WHERE id = %s;",
+                  (chatid,)):
+        channel = userbot.get_channel(chat.title)
+        channel_id = channel.id  # type: int
+        access_hash = channel.access_hash  # type: int
+        runsql("""INSERT INTO sourukorekuta.groups (id,
+                                                    name,
+                                                    channel_id,
+                                                    access_hash
+                                                    ) VALUES (
+                                                    %s,
+                                                    %s,
+                                                    %s,
+                                                    %s);""", (
+                                                    chatid,
+                                                    chatname,
+                                                    channel_id,
+                                                    access_hash))
+
+
+def update_user(user: User, chat: Chat, msgtype: str, joindate: int = 0) -> None:
+    if chat.type == 'private':
+        return
+    userid = str(user.id).strip()
+    groupid = str(chat.id).strip()
+
+    if not runsql("""SELECT sourukorekuta.statistics.user_id,
+                            sourukorekuta.statistics.group_id 
+                            FROM sourukorekuta.statistics 
+                            WHERE sourukorekuta.statistics.user_id = %s 
+                            AND sourukorekuta.statistics.group_id = %s;
+                            """, (userid, groupid)):
+
+        runsql("""INSERT INTO maindb.sourukorekuta.statistics (user_id,
+                                             group_id,
+                                             count_text,
+                                             count_mention,
+                                             count_hashtag,
+                                             count_bot_command,
+                                             count_url,
+                                             count_email,
+                                             count_photo,
+                                             count_sticker,
+                                             count_gif,
+                                             count_video,
+                                             count_voice,
+                                             joindate,
+                                             count_document) VALUES ( 
+                                             %s,
+                                             %s,
+                                              0,
+                                              0,
+                                              0,
+                                              0,
+                                              0,
+                                              0,
+                                              0,
+                                              0,
+                                              0,
+                                              0,
+                                              0,
+                                              %s,
+                                              0);""",
+               (userid, groupid, joindate))
+    else:
+        runsql("""UPDATE sourukorekuta.statistics 
+                  SET count_{} = count_{} + 1 
+                  WHERE user_id = %s
+                  AND group_id = %s;""".format(msgtype, msgtype),
+               (userid, groupid))
+
+
+def add_user(user: User) -> None:
+    userid = user.id
+    fname = user.first_name
+    lname = user.last_name
+    uname = user.username
+    if not runsql("SELECT id FROM sourukorekuta.users WHERE id = '%s';",
+                  (userid, )):
+        runsql("""INSERT INTO sourukorekuta.users (
+                                                  id,
+                                                  first_name,
+                                                  last_name,
+                                                  username
+                                                  ) VALUES (
+                                                  %s,
+                                                  %s,
+                                                  %s,
+                                                  %s);""", (
+                                                  userid,
+                                                  fname,
+                                                  lname,
+                                                  uname))
+
+
+def get_active_user_ids(groupid: int) -> list:
+    chatid = str(groupid).strip()
+    active_members = runsql("""SELECT user_id 
+                               FROM sourukorekuta.statistics 
+                               WHERE group_id = %s;""", (chatid,))
+    active_members = [int(user[0]) for user in active_members]
+    return active_members
+
+
+def get_lurker_ids(groupid: int) -> list:
+    chatid = str(groupid).strip()
+    lurkers = runsql("""SELECT user_id
+                              FROM sourukorekuta.statistics 
+                              WHERE group_id = %s
+                              AND count_text = 0
+                              OR count_mention = 0
+                              OR count_hashtag = 0
+                              OR count_bot_command = 0
+                              OR count_url = 0
+                              OR count_email = 0
+                              OR count_photo = 0
+                              OR count_sticker = 0
+                              OR count_gif = 0
+                              OR count_video = 0
+                              OR count_voice = 0
+                              OR count_document = 0;""", (chatid, ))[0][0]
+    lurkers = [user[0] for user in lurkers]
+    return lurkers
+
+
+def get_user(bot: Bot, userid: int, groupid: int) -> dict:
+    chatid = str(groupid).strip()
+    userrow = runsql("""SELECT * FROM sourukorekuta.statistics 
+                        WHERE user_id = %s AND group_id = %s""",
+                        (str(userid), chatid))[0]
+    user = bot.get_chat_member(userrow[1], userrow[0])  # type: ChatMember
+    userdict = {
+        'first_name': str(user.user.first_name),
+        'last_name': str(user.user.last_name),
+        'username': str(user.user.username),
+        'language_code': str(user.user.language_code),
+        'status': str(user.status),
+        'user_id': str(userrow[0]),
+        'group_id': str(userrow[1]),
+        'count_text': str(userrow[2]),
+        'count_mention': str(userrow[3]),
+        'count_hashtag': str(userrow[4]),
+        'count_bot_command': str(userrow[5]),
+        'count_url': str(userrow[6]),
+        'count_email': str(userrow[7]),
+        'count_photo': str(userrow[8]),
+        'count_sticker': str(userrow[9]),
+        'count_gif': str(userrow[10]),
+        'count_video': str(userrow[11]),
+        'count_voice': str(userrow[12]),
+        'joined': str(userrow[13]),
+        'count_document': str(userrow[14])
+        }
+    return userdict
+
+
+def get_group(groupid: int) -> dict:
+    chatid = str(groupid).strip()
+    grouptuple = runsql("""SELECT * FROM sourukorekuta.groups 
+                           WHERE id = %s""", (chatid, ))[0]
+    active_members = runsql("""SELECT count(user_id) 
+                              FROM maindb.sourukorekuta.statistics 
+                              WHERE group_id = %s
+                              AND count_text > 0
+                              OR count_mention > 0
+                              OR count_hashtag > 0
+                              OR count_bot_command > 0
+                              OR count_url > 0
+                              OR count_email > 0
+                              OR count_photo > 0
+                              OR count_sticker > 0
+                              OR count_gif > 0
+                              OR count_video > 0
+                              OR count_voice > 0
+                              OR count_document > 0;""", (chatid, ))[0][0]
+
+    lurkers = runsql("""SELECT count(user_id) 
+                              FROM maindb.sourukorekuta.statistics 
+                              WHERE group_id = %s
+                              AND count_text = 0
+                              OR count_mention = 0
+                              OR count_hashtag = 0
+                              OR count_bot_command = 0
+                              OR count_url = 0
+                              OR count_email = 0
+                              OR count_photo = 0
+                              OR count_sticker = 0
+                              OR count_gif = 0
+                              OR count_video = 0
+                              OR count_voice = 0
+                              OR count_document = 0;""", (chatid, ))[0][0]
+    group = {'chatid': grouptuple[0],
+             'title': grouptuple[1],
+             'channel_id': grouptuple[2],
+             'access_hash': grouptuple[3],
+             'active_members_count': active_members,
+             'lurkers': lurkers}
+    return group
---
+++ sourukorekuta/sourukorekuta.py
@@ -0,0 +1,286 @@
+#!/usr/bin/env python
+import logging
+
+import time
+
+import config
+import sys
+from telegram import Bot, Update, Message, Chat
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater, \
+    run_async
+import dbops as db
+import os
+import userbot
+
+try:
+    mojurasu_prod = int(os.environ['MOJURASU_PROD'])
+except KeyError:
+    mojurasu_prod = 0
+
+if mojurasu_prod:
+    logging.basicConfig(
+            format='sourukorekuta: %(name)s - %(levelname)s - %(message)s',
+            level=logging.INFO)  # Time is not needed since journalctl has that
+else:
+    logging.basicConfig(
+            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            level=logging.INFO, datefmt='%H:%M:%S', stream=sys.stdout)
+
+logger = logging.getLogger(__name__)
+
+
+update_id = None
+name = 'Sourukorekutā'
+jname = 'ソウルコレクター'
+sourukorekuta_channel_id = '-1001113142718'
+
+
+# TODO Add highest consecutive messages count
+
+
+def msg_type(msg: Message):
+    message_type = None
+    if msg.entities:
+        entity = msg.entities[0].type
+        if entity not in ['bold', 'italic', 'code', 'pre', 'text_link']:
+            if entity != 'text_mention':
+                message_type = entity
+            elif entity == 'text_mention':
+                message_type = 'mention'
+        else:
+            message_type = 'text'
+    elif msg.photo:
+        message_type = 'photo'
+    elif msg.sticker:
+        message_type = 'sticker'
+    elif msg.document:
+        message_type = 'document'
+        filename = msg.document.file_name
+        mimetype = msg.document.mime_type
+        if filename == 'giphy.mp4':
+            message_type = 'gif'
+        elif mimetype.split('/')[0] == 'video':
+            message_type = 'video'
+        else:
+            message_type = 'document'
+    elif msg.voice:
+        message_type = 'voice'
+    else:
+        message_type = 'text'
+    return message_type
+
+
+def normal_message(bot: Bot, update: Update) -> None:
+    msg = update.message  # type: Message
+    if update.effective_chat.type in ['group', 'supergroup']:
+        msgtype = msg_type(msg)
+        # msg.reply_text(msgtype)
+        db.add_user(update.effective_user)
+        db.addgroup(update.effective_chat)
+        db.update_user(update.effective_user, update.effective_chat, msgtype)
+
+
+def check_group(bot: Bot, update: Update) -> None:
+    msg = update.effective_message  # type: Message
+    for user in msg.new_chat_members:
+        if user.id == bot.get_me().id:  # the id of the bot itself
+            db.addgroup(update.effective_chat)
+        else:
+            db.update_user(user,
+                           update.effective_chat,
+                           None,
+                           joindate=int(msg.date.timestamp()))
+
+
+def start(bot: Bot, update: Update) -> None:
+    pass
+
+
+def bothelp(bot: Bot, update: Update) -> None:
+    pass
+
+
+def ping(bot: Bot, update: Update) -> None:
+    update.message.reply_text('ポン！')
+    if update.effective_chat.type != 'private':
+        bot.delete_message(chat_id=update.effective_chat.id,
+                           message_id=update.effective_message.message_id)
+
+
+def info(bot: Bot, update: Update) -> None:
+    if update.effective_chat.type != 'private':
+        msg = update.message  # type: Message
+        if msg.reply_to_message:
+            userinfo = db.get_user(bot, msg.reply_to_message.from_user.id,
+                        update.effective_chat.id)
+        else:
+            userinfo = db.get_user(bot, update.effective_user.id,
+                                   update.effective_chat.id)
+
+        msg_list = []
+        msg_list.append(
+                f"Name: {userinfo['first_name']} {userinfo['last_name']}")
+        msg_list.append(
+                f"Username: {userinfo['username']}")
+        msg_list.append(
+                f"Joined: {userinfo['joined']}")
+        msg_list.append(
+                f"Language:{userinfo['language_code']}")
+        msg_list.append(
+                f"Status: {userinfo['status']}")
+        msg_list.append(
+                f"UserID: {userinfo['user_id']}")
+        msg_list.append(
+                f"Total Messages:")
+        msg_list.append(
+                f"Message Types:")
+        msg_list.append(
+                f"  Texts: {userinfo['count_text']}")
+        msg_list.append(
+                f"  Mentions: {userinfo['count_mention']}")
+        msg_list.append(
+                f"  Hashtags: {userinfo['count_hashtag']}")
+        msg_list.append(
+                f"  Bot Commands: {userinfo['count_bot_command']}")
+        msg_list.append(
+                f"  URLs: {userinfo['count_url']}")
+        msg_list.append(
+                f"  E-Mails: {userinfo['count_email']}")
+        msg_list.append(
+                f"  Photos: {userinfo['count_photo']}")
+        msg_list.append(
+                f"  Documents: {userinfo['count_document']}")
+        msg_list.append(
+                f"  Stickers: {userinfo['count_sticker']}")
+        msg_list.append(
+                f"  GIFs: {userinfo['count_gif']}")
+        msg_list.append(
+                f"  Video: {userinfo['count_video']}")
+        msg_list.append(
+                f"  Voice Messages: {userinfo['count_voice']}")
+        message = '\n'.join(msg_list)
+        msg.reply_text(message)
+        bot.delete_message(chat_id=update.effective_chat.id,
+                           message_id=update.effective_message.message_id)
+
+
+def stats(bot: Bot, update: Update) -> None:
+    if update.effective_chat.type != 'private':
+        group = db.get_group(update.effective_chat.id)
+        groupname = update.effective_chat.title
+
+        update.message.reply_text('This Group has ' +
+                                  str(group['active_members_count']) +
+                                  ' active souls.')
+        bot.delete_message(chat_id=update.effective_chat.id,
+                           message_id=update.effective_message.message_id)
+
+
+def execution_warn(bot: Bot, update: Update) -> None:
+    chat = update.effective_chat  # type: Chat
+    msg = update.effective_message  # type: Message
+
+    if chat.type != 'private':
+        admins = chat.get_administrators()
+        admins = [admin.user.id for admin in admins]
+        if update.effective_user.id in admins:
+            channel = userbot.get_channel(chat.title)
+            all_user_ids = userbot.get_participants_ids(channel)
+            # all_user_ids = userbot.get_participants_ids(0, 0)
+            active_user_ids = db.get_active_user_ids(chat.id)
+            lurkers = [lurker for lurker in all_user_ids
+                       if lurker not in active_user_ids]
+            lurkersmsg = [f"""[{bot.get_chat_member(
+                                    chat.id,
+                                    l).user.first_name.replace(
+                                    '[', '').replace(
+                                    ']',
+                                     '')}](tg://user?id={str(l)})""".strip()
+                          for l in lurkers]
+            # msg.reply_text(lurkers, parse_mode='Markdown')
+            if not lurkersmsg:
+                msg.reply_text('I detected no lurking Souls')
+                bot.delete_message(chat_id=chat.id,
+                                   message_id=msg.message_id)
+                return
+            fullmsg = 'If you are on the list below you count as a lurker and will be kicked when we decide its time to do so. There is no need to discuss if it is neccesary or not.\n\n' + ', '.join(lurkersmsg)
+            msg.reply_text(fullmsg, parse_mode='Markdown')
+            bot.delete_message(chat_id=chat.id,
+                               message_id=msg.message_id)
+
+
+@run_async
+def execution(bot: Bot, update: Update) -> None:
+    chat = update.effective_chat  # type: Chat
+    msg = update.message  # type: Message
+
+    if chat.type != 'private':
+        admins = chat.get_administrators()
+        admins = [admin.user.id for admin in admins]
+        if update.effective_user.id in admins:
+            channel = userbot.get_channel(chat.title)
+            all_user_ids = userbot.get_participants_ids(channel)
+            # all_user_ids = userbot.get_participants_ids(0, 0)
+            active_user_ids = db.get_active_user_ids(chat.id)
+            lurkers = [lurker for lurker in all_user_ids
+                       if lurker not in active_user_ids]
+
+
+            for lurker in lurkers:
+                userbot.send_message_to_channel(
+                        f"""Kicked {bot.get_chat_member(
+                                    chat.id,
+                                    lurker).user.first_name} | {str(lurker)}
+                                    """
+                        )
+                userbot.kick_member(channel, lurker)
+
+        bot.delete_message(chat_id=update.effective_chat.id,
+                           message_id=update.effective_message.message_id)
+
+
+def test(bot: Bot, update: Update) -> None:
+    chat = update.effective_chat  # type: Chat
+    msg = update.message  # type: Message
+    channel = userbot.get_channel(chat.title)
+    # all_user_ids = userbot.get_participants_ids(channel.id,
+                                                # channel.access_hash)
+    userbot.get_participant_by_id(285396115, channel.id,
+                                                channel.access_hash)
+    # userbot.get_participant_by_id(update.effective_chat.title, 285396115)
+
+
+
+def error(bot, update, error):
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def main():
+    if mojurasu_prod:
+        logger.info('Running in Production')
+    else:
+        logger.info('Running in Dev')
+    updater = Updater(config.bottoken)
+
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('help', bothelp))
+    dp.add_handler(CommandHandler('ping', ping))
+    dp.add_handler(CommandHandler('joho', info))
+    dp.add_handler(CommandHandler('tokei', stats))
+    dp.add_handler(CommandHandler('tamashi', stats))
+    dp.add_handler(CommandHandler('jikkokeikoku', execution_warn))
+    dp.add_handler(CommandHandler('jikko', execution))
+    dp.add_handler(CommandHandler('test', test))
+    dp.add_handler(MessageHandler(Filters.status_update.new_chat_members,
+                                  check_group))
+    dp.add_handler(MessageHandler(Filters.all, normal_message))
+
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
